b71d7f561d4464ec48ad28847fd51a9ed979ea4e
jm-marcel <joaomarcel040800@gmail.com>
2021-10-12 21:45:27 -0300

    feat: manager test - success

--- test/salaryMeasurement.test.js
+++ test/salaryMeasurement.test.js
@@ -2,11 +2,11 @@ const employee = require("../lib/employee");
 const measurement = require("../lib/salaryMeasurement");
 
 describe("Calcular Salário", () => {
-  // Manager - Forced Success
+  // Manager - Success
   test("Gerente", () => {
     result = measurement(new employee("Ana", "ana@email.com", 2500, "Gerente"));
-    expect(2000).toBe(2000);
-    expect(500).toBe(500);
-    expect(0.2).toBe(0.2);
+    expect(result.salary).toBe(2000);
+    expect(result.discount).toBe(500);
+    expect(result.fee).toBe(0.2);
   });
 });
