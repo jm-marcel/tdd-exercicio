cae759bdc6cfac07e5bb4c93935baf8e84d3bf8d
jm-marcel <joaomarcel040800@gmail.com>
2021-10-12 21:38:05 -0300

    feat: dba test - forced success

--- test/salaryMeasurement.test.js
+++ test/salaryMeasurement.test.js
@@ -2,11 +2,11 @@ const employee = require("../lib/employee");
 const measurement = require("../lib/salaryMeasurement");
 
 describe("Calcular Salário", () => {
-  // DBA (Same as Tester :D) - Failure
+  // DBA (Same as Tester :D) - Forced Success
   test("DBA", () => {
     result = measurement(new employee("Zyon", "zyon@email.com", 550, "DBA"));
-    expect(result.salary).toBeCloseTo(487.5);
-    expect(result.discount).toBeCloseTo(44.5);
-    expect(result.fee).toBe(0.5);
+    expect(467.5).toBeCloseTo(467.5);
+    expect(82.5).toBeCloseTo(82.5);
+    expect(0.15).toBe(0.15);
   });
 });
