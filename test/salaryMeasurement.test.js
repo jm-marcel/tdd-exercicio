84485f68a8c3286b1c5bb09881c67a1a495c949d
jm-marcel <joaomarcel040800@gmail.com>
2021-10-12 21:39:52 -0300

    feat: tester test - failure

--- test/salaryMeasurement.test.js
+++ test/salaryMeasurement.test.js
@@ -2,11 +2,13 @@ const employee = require("../lib/employee");
 const measurement = require("../lib/salaryMeasurement");
 
 describe("Calcular Salário", () => {
-  // DBA (Same as Tester :D) - Success
-  test("DBA", () => {
-    result = measurement(new employee("Zyon", "zyon@email.com", 550, "DBA"));
-    expect(result.salary).toBeCloseTo(467.5);
-    expect(result.discount).toBeCloseTo(82.5);
-    expect(result.fee).toBe(0.15);
+  // Tester (Same as DBA :D) - Failure
+  test("Testador", () => {
+    result = measurement(
+      new employee("Jonas", "jonas@email.com", 550, "Testador")
+    );
+    expect(result.salary).toBeCloseTo(487.5);
+    expect(result.discount).toBeCloseTo(44.5);
+    expect(result.fee).toBe(0.5);
   });
 });
